1752c1b803a1a6fd45c0edbbeb39bc41ced96ca6
Eddy Perez <eddypv0@gmail.com>
2022-01-14 14:54:32 -0400

    create new types and interfaces

--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,41 @@ export enum Gender {
     MALE='male',
     FEMALE='female'
 }
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry{
-
+enum HealthCheckRating{
+    "Healthy"=0,
+    "LowRisk"=1,
+    "HighRisk"=2,
+    "CriticalRisk"=3
 }
+interface BaseEntry{
+    id:string,
+    description:string,
+    date:string,
+    specialist:string,
+    diagnosisCodes?:Array<Diagnose['code']>
+}
+interface Discharge{
+    date:string,
+    criteria:string
+}
+interface SickLeave{
+    startDate:string,
+    endDate:string,
+}
+interface HealthCheckEntry extends BaseEntry{
+    type:"HealthCheck",
+    healthCheckRating :HealthCheckRating
+}
+interface HospitalEntry extends BaseEntry{
+    type:"Hospital",
+    discharge: Discharge
+}
+interface OccupationalHealthCareEntry extends BaseEntry{
+    type:"OccupationalHealthCare",
+    sickLeave:SickLeave
+}
+type Entry = HealthCheckEntry | HospitalEntry | OccupationalHealthCareEntry;
+
 export interface Diagnose{
     code:string,
     name:string,
